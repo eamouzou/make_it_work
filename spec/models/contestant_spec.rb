59ef67e0e5423067be848312c43fbef58e054198
Elom Amouzou <elom1990@yahoo.com>
2020-02-07 11:05:50 -0700

    add to contestant model test for project_names instance method

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -13,4 +13,27 @@ RSpec.describe Contestant, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:projects).through(:contestant_projects)}
   end
+
+  describe "methods" do
+    describe "instance method" do
+      before(:each) do
+        @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+        @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+        @newsproject = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+        @boardproject = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+        @couchproject = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+        @lampproject = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+        @contestant1 = @newsproject.contestants.create(name: "bill", age: 5, hometown: "Hoboken", years_of_experience: 8)
+        @contestant7 = @lampproject.contestants.create(name: "aminata", age: 50, hometown: "Tambacounda", years_of_experience: 37)
+      end
+
+      it "can find and produce a string of project names it is connected to" do
+        expect(@contestant1.project_names).to eq("News Chic")
+        expect(@contestant7.project_names).to eq("Litfit")
+      end
+    end
+  end
 end
