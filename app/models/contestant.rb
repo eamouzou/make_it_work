bd65bbca34ff03d8f55cab0002f17599e2da36da
Elom Amouzou <elom1990@yahoo.com>
2020-02-07 09:15:53 -0700

    add validations to contestant

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -1,3 +1,6 @@
 class Contestant <ApplicationRecord
-
+  validates_presence_of :name
+  validates_presence_of :age
+  validates_presence_of :hometown
+  validates_presence_of :years_of_experience
 end
