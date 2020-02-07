cf7b1ba608f34e15f4f744448ccd9a7abf98e459
Elom Amouzou <elom1990@yahoo.com>
2020-02-07 11:31:01 -0700

    revise project show page test with create_contestants method

--- spec/features/projects/user_can_see_project_information_spec.rb
+++ spec/features/projects/user_can_see_project_information_spec.rb
@@ -10,6 +10,18 @@ RSpec.describe "project information", type: :feature do
 
     @couchproject = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
     @lampproject = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+    @contestant1 = @newsproject.create_contestants("bill", 5, "Hoboken", 8)
+    @contestant2 =  @newsproject.create_contestants("chronixx", 28, "Kingston", 20)
+
+    @contestant3 = @boardproject.create_contestants("malachi", 18, "Montbello", 2)
+    @contestant4 = @boardproject.create_contestants("javohn", 46, "Jackson", 30)
+
+    @contestant5 = @couchproject.create_contestants("uchekwukwu", 23, "Lagos", 10)
+    @contestant6 = @couchproject.create_contestants("valentina", 30, "Chiapas", 6)
+
+    @contestant7 =   @newsproject.create_contestants("aminata", 50, "Tambacounda", 37)
+    @contestant8 =   @newsproject.create_contestants("lee", 34, "Marseilles", 5)
   end
 
   scenario "visit project show page, see name, material and theme" do
