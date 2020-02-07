b55f76bd85ff2b8f278ce73ec784871eede35198
Elom Amouzou <elom1990@yahoo.com>
2020-02-07 10:19:36 -0700

    add instance method test to project test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,23 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe "methods" do
+    describe "instance method" do
+      before(:each) do
+        @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+        @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+        @newsproject = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+        @boardproject = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+        @couchproject = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+        @lampproject = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+      end
+
+      it "can find the theme it is connected to" do
+        expect(@lampproject.theme).to eq("Apartment Furnishings")
+      end
+    end
+  end
 end
